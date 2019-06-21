2451e917a8d4b0dd2fda21b22ef61468219291cf
Wes Jonke <wesjonke@gmail.com>
2019-06-21 13:40:49 -0400

    completed MVP, no erros, smurfs add to list and array is displayed properly

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,49 @@
+import axios from 'axios';
+
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
+
+export const getSmurfs = () => dispatch => {
+  dispatch ({ type: FETCH_SMURFS_START});
+    axios
+      .get('http://localhost:3333/smurfs/')
+      .then(res => {
+        dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data })
+      })
+      .catch (err => {
+        dispatch({
+          type: FETCH_SMURFS_FAIL,
+          payload: err.response.data.error.message
+        });
+      });
+}
+
+
+export const ADD_SMURFS_START = 'ADD_SMURFS_START';
+export const ADD_SMURFS_SUCCESS = 'ADD_SMURFS_SUCCESS';
+export const ADD_SMURFS_FAIL = 'ADD_SMURFS_FAIL';
+
+export const addSmurf = addsmurf => dispatch => {
+  dispatch ({ type: ADD_SMURFS_START});
+    axios
+      .post('http://localhost:3333/smurfs/',  addsmurf)
+      .then(res => {
+        dispatch({ type: ADD_SMURFS_SUCCESS, payload: res.data })
+      })
+      .catch (err => {
+        dispatch({
+          type: ADD_SMURFS_FAIL,
+          payload: err.response.data.error.message
+        });
+      });
+}
+
+
+
+
+
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
